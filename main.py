dc098c013ad15913395c26188063e3795dd64542
hask777 <haskellisp@gmail.com>
2021-03-04 18:47:43 +0400

    third part

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ API_KEY = 'AIzaSyBYcraWvyE6HXPS6bQ_j_jBmb9_Ua5axxk'
 channel_id = 'UCbXgNpp0jedKWcQiULLbDTA'
 
 yt = YTstats(API_KEY, channel_id)
-# yt.get_channel_statistics()
-# yt.dump()
+yt.get_channel_statistics()
 yt.get_channel_video_data()
+yt.dump()
+# yt.get_channel_video_data()
--- youtube_stat.py
+++ youtube_stat.py
@@ -1,6 +1,7 @@
 
 import json
 import requests
+from tqdm import tqdm
 
 class YTstats:
 
@@ -28,8 +29,28 @@ class YTstats:
         # 1) get video ids
         channel_videos = self._get_channel_videos(limit=50)
         print(len(channel_videos))
+        print(channel_videos)
 
         # 2)  get video statistics, TODO: next video
+        parts = ['snippet', 'video_id', 'part']
+        for video_id in tqdm(channel_videos):
+            for part in parts:
+                data = self._get_single_video_data(video_id, part)
+                channel_videos[video_id].uodate(data)
+
+        self.video_data = channel_videos
+        return channel_videos
+             
+    def _get_single_video_data(self, video_id, part):
+        url = f"https://www.googleapis.com/youtube/v3/videos?part={part}&id={video_id}&key={self.api_key}"
+        json_url = requests.get(url)
+        data = json.loads(json_url.text)
+        try:
+            data = data['items'][0][part]
+        except KeyError as e:
+            print(f'Error! Could not get {part} part of data: \n{data}')
+            data = dict()
+        return data
 
     def _get_channel_videos(self, limit=None):
         url = f'https://www.googleapis.com/youtube/v3/search?key={self.api_key}&channelId&id={self.channel_id}&part=id&order=date'
@@ -70,13 +91,16 @@ class YTstats:
         return channel_videos, nextPageToken
 
     def dump(self):
-        if self.channel_statistics is None:
+        if self.channel_statistics is None or self.video_data is None:
+            print('data is None')
             return
 
-        channel_title = "Python Engineer" # TODO: get channel name from data
+        fused_data = {self.channel_id:{"channel_statistics": self.channel_statistics, "video_data": self.video_data}}
+
+        channel_title = self.video_data.popitem()[1].get('channelTitle', self.channel_id)
         channel_title = channel_title.replace(" ", "_").lower()
         file_name = channel_title + '.json'
         with open(file_name, 'w') as f:
-            json.dump(self.channel_statistics, f, indent=4)
+            json.dump(fused_data, f, indent=4)
 
         print('file_dumped')
